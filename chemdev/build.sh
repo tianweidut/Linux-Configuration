358ae5a6e2f1450e73a679b48d44024ef53786d9
tianweidut <liutianweidlut@qq.com>
2012-12-30 10:36:44 +0000

    add chemistry dev & software script

---
+++ chemdev/build.sh
@@ -0,0 +1,23 @@
+##################
+#Chemistry tools
+
+#Web tools
+sudo pip install python-django -y   # Django
+sudo pip install lbforum                # Lbforum(论坛)
+sudo pip install django-debug-toolbar   # debug tools
+sudo apt-get install python-piston -y   # Piston, a RESTFul API 
+sudo pip install askbot                 # other necessary libs
+
+# create database
+python manage.py migrate
+python manage.py syncdb
+
+#Calculated software
+sudo apt-get install openbabel -y
+
+#dragon 6
+sudo apt-get install ia32-libs -y  #32-bit lib for 64-bit machine
+sudo apt-get install libc6 libx11-6 libgtk2.0-0 libglib2.0-0 libpango1.0-0 libcairo2 libatk1.0-0 libgl1-mesa-glx libsqlite3-0
+
+
+
