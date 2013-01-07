fdd552b9306fa2944192657a2943de5a38d88627
tianweidut <liutianweidlut@qq.com>
2013-01-07 11:34:17 +0000

    update build.sh for chem dev

--- chemdev/build.sh
+++ chemdev/build.sh
@@ -2,13 +2,16 @@
 #Chemistry tools
 
 #Web tools
-sudo pip install python-django -y   # Django
+sudo pip install python-django    # Django
 sudo pip install lbforum                # Lbforum(论坛)
 sudo pip install django-debug-toolbar   # debug tools
 sudo apt-get install python-piston -y   # Piston, a RESTFul API 
+sudo pip install tinymce
 sudo pip install askbot                 # other necessary libs
 
 # create database
+create database Chemistry CHARACTER SET utf8;
+python manage.py syncdb
 python manage.py migrate
 python manage.py syncdb
 
@@ -17,7 +20,6 @@ sudo apt-get install openbabel -y
 
 #dragon 6
 sudo apt-get install ia32-libs -y  #32-bit lib for 64-bit machine
-sudo apt-get install libc6 libx11-6 libgtk2.0-0 libglib2.0-0 libpango1.0-0 libcairo2 libatk1.0-0 libgl1-mesa-glx libsqlite3-0
-
+sudo apt-get install libc6 libx11-6 libgtk2.0-0 libglib2.0-0 libpango1.0-0 libcairo2 libatk1.0-0 libgl1-mesa-glx libsqlite3-0 -y
 
 
