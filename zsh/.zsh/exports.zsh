77b7a8ec47668d979d75189061692ab7fb1c8d75
tianweidut <liutianweidlut@qq.com>
2013-06-22 11:58:48 +0800

    update zsh and git

---
+++ zsh/.zsh/exports.zsh
@@ -0,0 +1,15 @@
+
+# Setup terminal, and turn on colors
+export CLICOLOR=1
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+
+# Enable color in grep
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='3;33'
+
+# Set LC_ALL="UTF8"
+export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+ 
